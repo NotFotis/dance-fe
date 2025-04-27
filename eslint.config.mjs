833043e4608ac6a67ef0c22229976db496133f76
f.staikos7 <f.staikos7@gmail.com>
2025-04-27 17:28:29 +0300

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,14 @@
-// eslint.config.mjs
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// necessary to get __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  // Next.js’s recommended web-vitals rules:
-  ...compat.extends("next/core-web-vitals"),
-  // Then TypeScript support:
-  ...compat.extends("next/typescript"),
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-  // If you need to add custom rules, append another object here:
-  // {
-  //   rules: {
-  //     "react/no-unescaped-entities": "off",
-  //     "@next/next/no-img-element": "off",
-  //   },
-  // },
-];
+export default eslintConfig;
